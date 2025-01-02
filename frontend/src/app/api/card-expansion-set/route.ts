2c81a706fa89ca36c7b77ebbe0941d3c4569eb6a
paulori22 <paulinhoboneti@yahoo.com.br>
2025-01-02 17:35:00 -0300

    Added order by code on cardExpansion api

--- frontend/src/app/api/card-expansion-set/route.ts
+++ frontend/src/app/api/card-expansion-set/route.ts
@@ -4,7 +4,9 @@ import { prisma } from "../../../../lib/prisma";
 export type CardExpansionSetApiResponse = CardExpasionSet[];
 
 export async function GET() {
-  const cardExpansionSets = await prisma.cardExpasionSet.findMany();
+  const cardExpansionSets = await prisma.cardExpasionSet.findMany({
+    orderBy: { code: "asc" },
+  });
 
   return Response.json(cardExpansionSets);
 }
